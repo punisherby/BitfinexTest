45f754894a0250670e357212726ac20b23ca5b12
Burak Yilmaz <burakyilmaza@gmail.com>
2020-10-11 12:34:33 +0300

    websocket integrated

--- src/ui/home/Home.js
+++ src/ui/home/Home.js
@@ -3,11 +3,45 @@ import React from 'react';
 import * as RootNavigation from '../RootNavigation.js';
 import {GLOBAL_ACTIONS} from '../../redux/actionTypes';
 import store from '../../redux/configureStore';
-import WS from 'react-native-websocket';
+
+const msg = JSON.stringify({
+    event: 'subscribe',
+    channel: 'ticker',
+    symbol: 'tBTCUSD'
+})
 
 export default class Home extends React.Component{
 
-    send = (data) => this.state.ws.send(data)
+    constructor() {
+        super();
+
+        this.socket = new WebSocket("wss://api-pub.bitfinex.com/ws/2");
+
+        this.socket.onopen = () => {
+            // connection opened
+            console.log("websocket started");
+            this.socket.send(msg); // send a message
+        };
+
+        this.socket.onmessage = (e) => {
+            // a message was received
+            if(!e.data.includes("hb")) {
+                console.log(e.data);
+            }
+        };
+
+        this.socket.onerror = (e) => {
+            // an error occurred
+            console.log("websocket error");
+            console.log(e.message);
+        };
+
+        this.socket.onclose = (e) => {
+            // connection closed
+            console.log("websocket closed");
+            console.log(e.code, e.reason);
+        };
+    }
 
     componentDidMount() {
         store.dispatch({ type: GLOBAL_ACTIONS.GET_TICKER_REQUEST, payload: "ALL" });
@@ -16,18 +50,6 @@ export default class Home extends React.Component{
     render() {
         return (
             <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-                <WS
-                    ref={ref => {this.ws = ref}}
-                    url="wss://echo.websocket.org/"
-                    onOpen={() => {
-                        console.log('Open!')
-                        this.ws.send('Hello')
-                    }}
-                    onMessage={console.log}
-                    onError={console.log}
-                    onClose={console.log}
-                    reconnect // Will try to reconnect onClose
-                />
                 <Text>Home</Text>
                 <TouchableOpacity onPress={() => RootNavigation.navigate('Details', { userName: 'Lucy' })}>
                     <Text>Click to view Details</Text>
