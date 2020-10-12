b0cb95a91bc72a587322f77f9d5ae45574542de9
Burak Yilmaz <burakyilmaza@gmail.com>
2020-10-12 16:28:49 +0300

    corrections

--- src/redux/sagas/apis.js
+++ src/redux/sagas/apis.js
@@ -1,7 +1,3 @@
-const headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-};
 import * as axios from 'axios';
 
 class apis {
@@ -9,49 +5,6 @@ class apis {
     getTicker = (symbol) => {
         return axios.get(`https://api-pub.bitfinex.com/v2/tickers?symbols=${symbol}`);
     };
-
-    updateProgramPhase = (body) => {
-        return fetch({
-            method: 'PUT',
-            resolvePath: (authInfo) => `/users/${authInfo.uuid}/customer/program_phase`,
-            requiresAuth: true,
-            body: body,
-            errorInfo: {
-                title: 'Update Program Phases Failed!',
-            },
-        });
-    };
-
-    getEmotions = () => {
-        return fetch({
-            method: 'GET',
-            path: `/lookups/emotions`,
-            requiresAuth: true,
-            loggingOption: {
-                logRequest: false,
-                logResponse: false,
-            },
-            errorInfo: {
-                title: 'Get Emotion Failed!',
-            },
-        });
-    };
-
-    assessPassword(data) {
-        return fetch({
-            method: 'POST',
-            path: '/lookups/assess_password',
-            body: data,
-            errorInfo: {
-                title: 'Assess password failed!',
-            },
-            loggingOption: {
-                logRequest: false,
-                logResponse: false,
-            },
-            informEventBus: false,
-        });
-    }
 }
 
 export default new apis();
